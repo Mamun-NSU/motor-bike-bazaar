ab321611cdf1d6e3144dc817eb130d45d5c27b99
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-05-09 00:31:32 +0600

    show all the products from mongoDB server successfully

--- src/components/Home/Product/Product.js
+++ src/components/Home/Product/Product.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Product.css";
-const Product = () => {
+const Product = ({ product }) => {
+  const { _id, name, image, about, price, quantity, supplier_name } = product;
+  const navigate = useNavigate();
+
+  const navigateToServiceDetail = id => {
+    navigate(`/products/${id}`);
+  }
   return (
-    <div className="w-75 mx-auto">
-      <h2>This is Product Section</h2>
+    <div className='product'>
+      <img className='w-100' src={image} alt="" />
+      <h2>{name}</h2>
+      <p>Price: {price}</p>
+      <p>Quantity: {quantity}</p>
+      <p>Supplier Name: {supplier_name}</p>
+      <p><small>{about}</small></p>
+      <button onClick={() => navigateToServiceDetail(_id)} className='btn btn-primary'>Book: {name}</button>
     </div>
   );
 };
