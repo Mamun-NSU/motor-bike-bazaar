5ab3a0cde67ac12434946303ac529fc975b2a311
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-05-08 07:52:46 +0600

    Header section updated successfully

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,71 +1,37 @@
-import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import "./Header.css";
-const Header = () => {
-  return (
-    <div>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="home">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="blogs">
-                Blogs
-              </Nav.Link>
-              <Nav.Link as={Link} to="checkout">
-                Checkout
-              </Nav.Link>
-              <Nav.Link as={Link} to="about">
-                About
-              </Nav.Link>
-            </Nav>
-            <Nav>
-              <Nav.Link as={Link} to="about">
-                About
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
-  );
-};
-
-export default Header;
-
 // import React from "react";
 // import { Container, Nav, Navbar } from "react-bootstrap";
-// import "./Header.css";
 // import { Link } from "react-router-dom";
-// import { useAuthState } from "react-firebase-hooks/auth";
-// import auth from "../../../firebase.init";
-
-// import { signOut } from "firebase/auth";
-
+// // import { Link } from "react-router-dom";
+// import "./Header.css";
 // const Header = () => {
-//   const [user] = useAuthState(auth);
-
-//   const handleSignOut = () => {
-//     signOut(auth);
-//   };
 //   return (
-//     <>
-//       <Navbar
-//         collapseOnSelect
-//         expand="lg"
-//         sticky="top"
-//         bg="primary"
-//         variant="dark"
-//       >
+//     <div>
+//       {/* <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 //         <Container>
-//           <Navbar.Brand as={Link} to="/"></Navbar.Brand>
+//           <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
 //           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+//           <Navbar.Collapse id="responsive-navbar-nav">
+//             <Nav className="me-auto">
+//               <Nav.Link href="#home">Home</Nav.Link>
+//               <Nav.Link href="#about">About</Nav.Link>
+//               <Nav.Link as={Link} to="blogs">
+//                 Blogs
+//               </Nav.Link>
+//             </Nav>
+//             <Nav>
+//               <Nav.Link href="#deets">More deets</Nav.Link>
+//               <Nav.Link eventKey={2} href="#memes">
+//                 Dank memes
+//               </Nav.Link>
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Container>
+//       </Navbar> */}
+//       {/* <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+//         <Container>
+//           <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+//           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
 //           <Navbar.Collapse id="responsive-navbar-nav">
 //             <Nav className="me-auto">
 //               <Nav.Link as={Link} to="home">
@@ -74,8 +40,8 @@ export default Header;
 //               <Nav.Link as={Link} to="blogs">
 //                 Blogs
 //               </Nav.Link>
-//               <Nav.Link as={Link} to="manageItems">
-//                 Manage Items
+//               <Nav.Link as={Link} to="checkout">
+//                 Checkout
 //               </Nav.Link>
 //               <Nav.Link as={Link} to="about">
 //                 About
@@ -85,25 +51,81 @@ export default Header;
 //               <Nav.Link as={Link} to="about">
 //                 About
 //               </Nav.Link>
-
-//               {user ? (
-//                 <button
-//                   className="btn btn-link text-white text-decoration-none"
-//                   onClick={handleSignOut}
-//                 >
-//                   sign out
-//                 </button>
-//               ) : (
-//                 <Nav.Link as={Link} to="login">
-//                   Login
-//                 </Nav.Link>
-//               )}
 //             </Nav>
 //           </Navbar.Collapse>
 //         </Container>
-//       </Navbar>
-//     </>
+//       </Navbar> */}
+//     </div>
 //   );
 // };
 
 // export default Header;
+
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+
+import { signOut } from "firebase/auth";
+
+const Header = () => {
+  const [user] = useAuthState(auth);
+
+  const handleSignOut = () => {
+    signOut(auth);
+  };
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        sticky="top"
+        bg="primary"
+        variant="dark"
+      >
+        <Container>
+          <Navbar.Brand as={Link} to="/"></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="home">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="blogs">
+                Blogs
+              </Nav.Link>
+              <Nav.Link as={Link} to="manageItems">
+                Manage Items
+              </Nav.Link>
+              <Nav.Link as={Link} to="about">
+                About
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link as={Link} to="about">
+                About
+              </Nav.Link>
+
+              {user ? (
+                <button
+                  className="btn btn-link text-white text-decoration-none"
+                  onClick={handleSignOut}
+                >
+                  sign out
+                </button>
+              ) : (
+                <Nav.Link as={Link} to="login">
+                  Login
+                </Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
