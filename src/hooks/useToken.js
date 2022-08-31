fc2008d9d2caa7659d207c340c0591ed417cbc21
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-08-31 23:53:00 +0600

    Updated successfully

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
-
+// This is use token hook
 const useToken = user => {
   const [token, setToken] = useState('');
   useEffect(() => {
